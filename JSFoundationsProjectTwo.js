cc107599aee922b3b5a87f71b40f7335d6f78acd
Thezigzagtree <33034701+Thezigzagtree@users.noreply.github.com>
2018-11-15 12:06:02 +0300

    Project Two Done

--- JSFoundationsProjectTwo.js
+++ JSFoundationsProjectTwo.js
@@ -14,7 +14,7 @@
  *
  */
 function filterEvens(numbers) {
-	// your code goes here!
+   return numbers.filter( number => { return number % 2 === 0 });
 }
 
 /**
@@ -29,7 +29,7 @@ function filterEvens(numbers) {
  *
  */
 function filterOdds(numbers) {
-	// your code goes here!
+    return numbers.filter(number => { return number % 2 !== 0 });
 }
 
 /**
@@ -42,8 +42,11 @@ function filterOdds(numbers) {
  *     returns 9
  *
  */
+
+// BONUS: use .reduce() for the sumOdds() and numberOfStateNamesContaining() functions
+// BONUS: Do sumOdds() without using the filterOdds() function and without using a .filter() method.
 function sumOdds(numbers) {
-	// your code goes here!
+    return numbers.reduce((acc, cur) => { if (cur % 2 !== 0) { return acc + cur; } else { return acc } });
 }
 
 
@@ -64,8 +67,17 @@ let states = ['Alabama','Alaska','American Samoa','Arizona','Arkansas','Californ
  * 				 'Northern Mariana Islands' ]
  *
  */
-function stateNamesLongerThan(minLength) {
-	// your code goes here!
+
+function removeSpacesFromNames(state)
+{
+    return state.replace(" ", "");
+}
+
+// BONUS: Do the functions stateNamesLongerThan() and numberOfStateNamesLongerThan() each in a single line.
+function stateNamesLongerThan(minLength)
+{
+    //I Added the function removeSpacesFromNames to make sure only the state name is taken into account without the additional spaces in between
+    return states.filter(state => { return removeSpacesFromNames(state).length > minLength });
 }
 
 /**
@@ -78,8 +90,10 @@ function stateNamesLongerThan(minLength) {
  *     returns 18
  *
  */
+
+// BONUS: Do the functions stateNamesLongerThan() and numberOfStateNamesLongerThan() each in a single line.
 function numberOfStateNamesLongerThan(minLength) {
-	// your code goes here!
+    return states.filter(state => { return state.length > minLength }).length;
 }
 
 /**
@@ -107,8 +121,9 @@ function numberOfStateNamesLongerThan(minLength) {
  *				  'Wyoming' ]
  *
  */
-function replaceStateNames(newName, len) {
-	// your code goes here!
+function replaceStateNames(newName, len)
+{
+    return states.map(state => { if (state.length === len) { return state.replace(state, newName); } else { return state; }});	
 }
 
 /**
@@ -121,10 +136,16 @@ function replaceStateNames(newName, len) {
  *     returns 4
  *
  */
-function numberOfStateNamesContaining(subString) {
-	// your code goes here!
-}
 
+// BONUS: use .reduce() for the sumOdds() and numberOfStateNamesContaining() functions
+function numberOfStateNamesContaining(subString) 
+{
+//    return states.filter(state => { return state.includes(subString) }).length;
+
+    let count = 0;
+    return states.reduce((acc, cur) => { if (cur.includes(subString)) { return count++; } else { return count; } });
+
+}
 
 /**************************************************
 The following code runs the functions defined above
@@ -132,38 +153,46 @@ The following code runs the functions defined above
 let numbers = [153,67,9,34,7,67,342,7,34,8656,2,124,5,43];
 
 // // filterEvens
-// let evens = filterEvens(numbers);
-// console.log("EVENS:");
-// console.log(evens);
+ let evens = filterEvens(numbers);
+ console.log("EVENS:");
+ console.log(evens);
 
 // // sumOdds
-// let sum = sumOdds(numbers);
-// console.log("\n--------------------------")
-// console.log("SUM ODDS:");
-// console.log(sum);
+ let sum = sumOdds(numbers);
+ console.log("\n--------------------------")
+ console.log("SUM ODDS:");
+ console.log(sum);
 
 // // stateNamesLongerThan
-// let largeNames = stateNamesLongerThan(15);
-// console.log("\n--------------------------")
-// console.log("STATE NAMES LONGER THAN:");
-// console.log(largeNames);
+ let largeNames = stateNamesLongerThan(15);
+ console.log("\n--------------------------")
+ console.log("STATE NAMES LONGER THAN:");
+ console.log(largeNames);
 
 // // numberOfStateNamesLongerThan
-// let numOfLargeStateNames = numberOfStateNamesLongerThan(10);
-// console.log("\n--------------------------")
-// console.log("NUMBER OF STATE NAMES LONGER THAN:");
-// console.log(numOfLargeStateNames);
+ let numOfLargeStateNames = numberOfStateNamesLongerThan(10);
+ console.log("\n--------------------------")
+ console.log("NUMBER OF STATE NAMES LONGER THAN:");
+ console.log(numOfLargeStateNames);
 
 // // replaceStateNames
-// let newNames = replaceStateNames("REBOUND", 12);
-// console.log("\n--------------------------")
-// console.log("REPLACE STATE NAMES:");
-// console.log(newNames);
+ let newNames = replaceStateNames("REBOUND", 12);
+ console.log("\n--------------------------")
+ console.log("REPLACE STATE NAMES:");
+ console.log(newNames);
 
 // // numberOfStateNamesContaining
-// let occurances = numberOfStateNamesContaining("New");
-// console.log("\n--------------------------")
-// console.log("NUMBER OF STATE NAMES CONTAININGa:");
-// console.log(occurances);
+ let occurances = numberOfStateNamesContaining("New");
+ console.log("\n--------------------------")
+ console.log("NUMBER OF STATE NAMES CONTAINING:");
+ console.log(occurances);
+
+ let spaceStates = numberOfStateNamesContaining(" ");
+ console.log("\n--------------------------")
+ console.log("NUMBER OF STATE NAMES CONTAINING:");
+ console.log(spaceStates);
 
 // How can you know the number of states with names that contain multiple words?
+// return states.filter(state => {return state.trim().includes(" ")}),length);
+// This is essentially like using the function "numberOfStateNamesContaining" with " " as the input.
+// I also would Trim the state name first to make sure there are no extra spaces on the edges, then see if the trimmed string has a space in the middle and return the length of that array.
